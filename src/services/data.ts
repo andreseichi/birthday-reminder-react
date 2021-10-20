25d377d9d301a3791c87bdab790acbd653c698bc
André Tashiro <andre_seichi@hotmail.com>
2021-10-20 18:09:30 -0300

    data

---
+++ src/services/data.ts
@@ -0,0 +1,79 @@
+export const data = [
+  {
+    id: 1,
+    name: 'André Tashiro',
+    birthday: '24/06/1999',
+    image:
+      'https://cdn.discordapp.com/avatars/394191672348508182/9fbd27cc5294358dafd28d58b72f9fb7.webp?size=128',
+  },
+  {
+    id: 2,
+    name: 'Fabiana Carrera',
+    birthday: '21/12/1998',
+    image:
+      'https://cdn.discordapp.com/attachments/831183446725361744/900148909341569065/pp.png',
+  },
+  {
+    id: 3,
+    name: 'Sonya',
+    birthday: '12/04/1995',
+    image:
+      'https://cdn.discordapp.com/attachments/831183446725361744/900149113037942834/pp.png',
+  },
+  {
+    id: 4,
+    name: 'Raquel Larrat',
+    birthday: '15/01/2000',
+    image:
+      'https://cdn.discordapp.com/attachments/831183446725361744/900147996648423464/photo.jpg',
+  },
+  {
+    id: 5,
+    name: 'Ricardo Martins',
+    birthday: '31/08/1999',
+    image:
+      'https://cdn.discordapp.com/attachments/831183446725361744/900148781364949012/pp.png',
+  },
+  {
+    id: 6,
+    name: 'João Fonseca',
+    birthday: '07/03/2000',
+    image:
+      'https://cdn.discordapp.com/avatars/330730699072208896/56eda6158135fc135958fa89a2f2393d.webp?size=128',
+  },
+  {
+    id: 7,
+    name: 'Breno Martins',
+    birthday: '12/12/2000',
+    image:
+      'https://cdn.discordapp.com/avatars/295597251663757312/5dbc353201410069048f1b6c1e1bc931.webp?size=128',
+  },
+  {
+    id: 8,
+    name: 'Lucas',
+    birthday: '13/10/1998',
+    image:
+      'https://cdn.discordapp.com/avatars/283058412273795073/388ffe0e909caa977c9e506c2bb9a9b1.webp?size=128',
+  },
+  {
+    id: 9,
+    name: 'Carlos Nascimento',
+    birthday: '16/11/1997',
+    image:
+      'https://cdn.discordapp.com/avatars/251907128371970048/c0c1d7563e310c30137d2142be2d9844.webp?size=128',
+  },
+  {
+    id: 10,
+    name: 'Eduarda Calderan',
+    birthday: '11/05/2002',
+    image:
+      'https://cdn.discordapp.com/avatars/762462580408582164/395b3bd34ca9710b8fa5d3201bb16618.webp?size=128',
+  },
+  {
+    id: 11,
+    name: 'Ploita',
+    birthday: '07/01/2000',
+    image:
+      'https://cdn.discordapp.com/avatars/276440527380152320/d420c9ef86740aabdbd38acb9de90eb1.webp?size=128',
+  },
+];
